d004ed942c3620ba04f58ee2916906f34d64f64b
Abhishek Gupta <86161191+Im-Abhi@users.noreply.github.com>
2026-03-02 19:05:53 +0530

    visual changes add pass/ fail coloring

--- lib/common.sh
+++ lib/common.sh
@@ -1,16 +1,28 @@
 #!bin/bash
 
+if [[ -t 1 ]]; then
+    GREEN='\033[0;32m'
+    RED='\033[0;31m'
+    YELLOW='\033[1;33m'
+    NC='\033[0m'  # No Color
+else
+    GREEN=''
+    RED=''
+    YELLOW=''
+    NC=''
+fi
+
 PASS=0
 FAIL=0
 
 pass() {
-    echo "[PASS] $1"
-    ((PASS++)) 
+    echo -e "${GREEN}[PASS]${NC} $1"
+    ((PASS++))
 }
 
 fail() {
-    echo "[FAIL] $1"
-    ((FAIL++)) 
+    echo -e "${RED}[FAIL]${NC} $1"
+    ((FAIL++))
 }
 
 apply_fix() {
